da1f2a7de9df81549df76c787dc54237ad71812b
margonaut <margaret.morgane@gmail.com>
2015-08-15 10:26:07 -0400

    Added a method to find episode description

--- test.rb
+++ test.rb
@@ -2,10 +2,22 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-page = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/List_of_Steven_Universe_episodes"))
+page = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/List_of_Trailer_Park_Boys_episodes"))
 
 class Episode
-  def initialize (title, number)
+  def initialize (title, number, description)
+  end
+end
+#
+def find_description(episode)
+  unless episode.next_element.nil?
+    description = episode.next_element.text
+  end
+  if description.nil?
+    description = "SOMETHING WEIRD IS HAPPENING HERE"
+  end
+  unless description.include? "TBA"
+    description
   end
 end
 
@@ -13,10 +25,27 @@ episodes = page.css('tr.vevent')
 episodes.each do |episode|
   title = (episode > ('.summary')).text
   number = episode.children[1].text
-  Episode.new(title, number)
+  description = find_description(episode)
+  Episode.new(title, number, description)
 end
 
 
+
+# episodes.each do |episode|
+#   unless episode.next_element.nil?
+#     description = episode.next_element.text
+#   end
+#   if description.nil?
+#     description = "SOMETHING WEIRD IS HAPPENING HERE"
+#   end
+#   unless description.include? "TBA"
+#     puts description
+#   end
+# end
+
+#
+# descriptions = page.css('tr.vevent').first.class
+# puts descriptions
 
 # episodes = page.css('tr.vevent')
 # puts episodes[0]
