5925b2c26044cb5cb2bbe4a256f934638f4646dd
margonaut <margaret.morgane@gmail.com>
2015-08-15 10:32:33 -0400

    Improved Episode class, made class vars readable

--- test.rb
+++ test.rb
@@ -5,7 +5,11 @@ require 'open-uri'
 page = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/List_of_Trailer_Park_Boys_episodes"))
 
 class Episode
+  attr_reader :title, :number, :description
   def initialize (title, number, description)
+    @title = title
+    @number = number
+    @description = description
   end
 end
 #
@@ -21,29 +25,20 @@ def find_description(episode)
   end
 end
 
+episode_list = []
 episodes = page.css('tr.vevent')
 episodes.each do |episode|
   title = (episode > ('.summary')).text
   number = episode.children[1].text
   description = find_description(episode)
-  Episode.new(title, number, description)
+  new_episode = Episode.new(title, number, description)
+  episode_list << new_episode
 end
 
+puts episode_list[0]
 
 
-# episodes.each do |episode|
-#   unless episode.next_element.nil?
-#     description = episode.next_element.text
-#   end
-#   if description.nil?
-#     description = "SOMETHING WEIRD IS HAPPENING HERE"
-#   end
-#   unless description.include? "TBA"
-#     puts description
-#   end
-# end
 
-#
 # descriptions = page.css('tr.vevent').first.class
 # puts descriptions
 
